f34051d7e704978bad5ba7cf203f0176b151a383
banover <75075908+banover@users.noreply.github.com>
2020-12-06 02:18:43 +0900

    php..

---
+++ correct.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>그림자-오늘의 시작</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+  <h1><a href="index.php">Soo's Container</a></h1>
+  <div class="grid">
+   <div style="border-right:3px solid black" class="active">오늘의 시작</div>
+   <a href="2.html" style="border-right:3px solid black">오늘의 만남</a>
+   <a href="3.html" style="border-right:3px solid black">오늘의 코드</a>
+   <a href="5.html" style="border-right:3px solid black">오늘의 사진</a>
+   <a href="4.html">오늘의 끝</a>
+  </div>
+
+<h3>오늘의 시작</h3>
+<img src="sunny.svg" >  <!--아침 그림 넣기-->
+<br><br>
+<h4>오늘 아침을 한 줄로 표현한다면?</h4>
+
+<hr>
+
+<a href="writing.html">기록하기</a>
+<?php if(isset($_GET['date'])){ ?>
+<a href="correct.php">수정하기</a>
+<?php } ?>
+<br>
+
+<form action="correct_process.php" method="post">
+
+  <div class="datainput">
+  <input id="date" type="date" name="date"style="width:125px; margin-right:10px;" value="">
+  <input id="realtext" type="text" name="description" placeholder="한 줄글 입력" >
+  <input id="action" type="submit" value="저장" style="width:56px;" >
+  </div>
+</form>
+
+
+
+
+
+
+<?php
+
+$oneline = scandir('./data');
+$i = 0;
+
+while ($i<count($oneline)){
+  if($oneline[$i] != '.'){
+    if($oneline[$i] != '..'){
+
+echo $oneline[$i];
+echo file_get_contents("data/".$oneline[$i]);
+echo '<br>';
+
+
+}
+}
+$i=$i+1;
+}
+
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ correct_process.php
@@ -0,0 +1,5 @@
+<?php
+
+file_put_contents('data/'.$_POST['date'], $_POST['description']);
+header('Location:/1.php');
+ ?>
